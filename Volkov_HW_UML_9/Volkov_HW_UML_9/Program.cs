98a724c9f7cc859804a7857dafba40093c9470b0
Landic <danil.volkov.235@gmail.com>
2023-03-27 11:52:19 +0300

    Добавил дз(1) за 27.03.2023

---
+++ Volkov_HW_UML_9/Volkov_HW_UML_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RPG obj = new Human();
+            obj = new HumanWarior(obj);
+            Console.WriteLine(obj.GetName() + " " + obj.GetHealth() + " " + obj.GetArmor() + " " + obj.GetAttack() + " " + obj.GetSpeed());
+            RPG obj2 = new Elf();
+            obj2 = new EvilMage(obj2);
+            Console.WriteLine(obj2.GetName() + " " + obj2.GetHealth() + " " + obj2.GetArmor() + " " + obj2.GetAttack() + " " + obj2.GetSpeed());
+        }
+    }
+}
---
+++ Volkov_HW_UML_9/Volkov_HW_UML_9/RPG.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_9
+{
+    internal abstract class RPG
+    {
+        protected string name;
+        public RPG(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public abstract int GetAttack();
+
+        public abstract int GetSpeed();
+
+        public abstract int GetHealth();
+
+        public abstract int GetArmor();
+    }
+
+    internal class Human : RPG
+    {
+        public Human() : base("Human")
+        {
+
+        }
+        public override int GetArmor()
+        {
+            return 0;
+        }
+
+        public override int GetAttack()
+        {
+            return 20;
+        }
+
+        public override int GetHealth()
+        {
+            return 150;
+        }
+
+        public override int GetSpeed()
+        {
+            return 20;
+        }
+    }
+
+    internal class Elf : RPG
+    {
+        public Elf() : base("Elf")
+        {
+
+        }
+        public override int GetArmor()
+        {
+            return 0;
+        }
+
+        public override int GetAttack()
+        {
+            return 15;
+        }
+
+        public override int GetHealth()
+        {
+            return 100;
+        }
+
+        public override int GetSpeed()
+        {
+            return 30;
+        }
+    }
+
+
+    internal abstract class HumanProffesion : RPG
+    {
+        protected RPG obj;
+
+        public HumanProffesion(string name,RPG obj) : base(name)
+        {
+            this.obj = obj;
+        }
+    }
+
+    internal class HumanWarior : HumanProffesion
+    {
+        public HumanWarior(RPG obj2) : base("Human Warior",obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 20;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 20;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 50;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 10;
+        }
+    }
+
+
+    internal class Swordsman : HumanProffesion
+    {
+        public Swordsman(RPG obj2) : base("Swordsman", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 20;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 40;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 50;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() - 10;
+        }
+    }
+
+    internal class Archer : HumanProffesion
+    {
+        public Archer(RPG obj2) : base("Archer", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 10;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 20;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 50;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 20;
+        }
+    }
+
+
+    internal class Rider : HumanProffesion
+    {
+        public Rider(RPG obj2) : base("Rider", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 100;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() - 10;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 200;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 40;
+        }
+    }
+
+
+    internal abstract class ElfProffesion : RPG
+    {
+        protected RPG obj;
+
+        public ElfProffesion(string name, RPG obj) : base(name)
+        {
+            this.obj = obj;
+        }
+    }
+
+    internal class ElfWarior : ElfProffesion
+    {
+        public ElfWarior(RPG obj2) : base("Elf Warior", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 20;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 20;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 100;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() - 10;
+        }
+    }
+
+
+    internal class ElfMagician : ElfProffesion
+    {
+        public ElfMagician(RPG obj2) : base("Elf Magician", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 20;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 10;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() - 50;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 10;
+        }
+    }
+
+
+    internal class Сrossbowman : ElfProffesion
+    {
+        public Сrossbowman(RPG obj2) : base("Сrossbowman", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() - 10;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 20;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 50;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 10;
+        }
+    }
+
+
+
+    internal class EvilMage : ElfProffesion
+    {
+        public EvilMage(RPG obj2) : base("Evil Mage", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 0;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 70;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 0;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 20;
+        }
+    }
+
+
+    internal class GoodMage : ElfProffesion
+    {
+        public GoodMage(RPG obj2) : base("Good Mage", obj2)
+        {
+        }
+
+        public override int GetArmor()
+        {
+            return obj.GetArmor() + 30;
+        }
+
+        public override int GetAttack()
+        {
+            return obj.GetAttack() + 50;
+        }
+
+        public override int GetHealth()
+        {
+            return obj.GetHealth() + 100;
+        }
+
+        public override int GetSpeed()
+        {
+            return obj.GetSpeed() + 30;
+        }
+    }
+}
