12af6debe097d341c61a029f18988677208033b0
Landic <danil.volkov.235@gmail.com>
2023-03-28 12:44:21 +0300

    Добавил дз за 28.03.2023

---
+++ Volkov_HW_UML_11/Volkov_HW_UML_11/MilitaryBase.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_11
+{
+    internal abstract class MilitaryBase
+    {
+        public int Move { get; set; }
+        public int Power { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Move -> {Move}\nPower -> {Power}");
+        }
+    }
+
+
+    internal class LightInfantry : MilitaryBase
+    {
+        public LightInfantry()
+        {
+            Move = 20;
+            Power = 10;
+        }
+    }
+
+    internal class Transport : MilitaryBase
+    {
+        public Transport()
+        {
+            Move = 70;
+            Power = 0;
+        }
+    }
+
+    internal class HeavyTransport : MilitaryBase
+    {
+        public HeavyTransport()
+        {
+            Move = 15;
+            Power = 150;
+        }
+    }
+
+    internal class LightTransport : MilitaryBase
+    {
+        public LightTransport()
+        {
+            Move = 50;
+            Power = 30;
+        }
+    }
+
+    internal class Airplane : MilitaryBase
+    {
+        public Airplane()
+        {
+            Move = 300;
+            Power = 100;
+        }
+    }
+
+    internal class MilitaryFactory
+    {
+        private List<MilitaryBase> list;
+
+        public MilitaryFactory()
+        {
+            list = new List<MilitaryBase>();
+        }
+
+        public void Show(MilitaryBase obj)
+        {
+            if(!list.Contains(obj))
+            {
+                list.Add(obj);
+            }
+            list.Where(i => i == obj).FirstOrDefault().Show();
+        }
+
+        public void ShowAll()
+        {
+            foreach(var i in list)
+            {
+                Show(i);
+            }
+        }
+    }
+}
---
+++ Volkov_HW_UML_11/Volkov_HW_UML_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilitaryFactory factory = new MilitaryFactory();
+            MilitaryBase obj = new HeavyTransport();
+            MilitaryBase obj2 = new Airplane();
+            MilitaryBase obj3 = new Airplane();
+            factory.Show(obj);
+            factory.Show(obj2);
+            factory.ShowAll();
+            factory.Show(obj3);
+            Console.WriteLine("------------------------");
+            factory.ShowAll();
+        }
+    }
+}
