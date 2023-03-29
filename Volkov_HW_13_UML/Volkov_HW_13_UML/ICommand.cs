22798263c2682ded22452920a1d1923ada8b9c65
Landic <danil.volkov.235@gmail.com>
2023-03-29 14:13:42 +0300

    Добавил дз(2) за 29.03.2023

---
+++ Volkov_HW_13_UML/Volkov_HW_13_UML/ICommand.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Volkov_HW_13_UML
+{
+    internal interface ICommand
+    {
+        void Excute();
+        void Undo();
+    }
+
+
+    internal class Notebook
+    {
+        public void On()
+        {
+            Console.WriteLine("Notebook ON");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Notebook OFF");
+        }
+    }
+
+    internal class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("TV ON");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("TV OFF");
+        }
+    }
+
+    internal class Microwave
+    {
+        public void Start(int time)
+        {
+            Console.WriteLine("Warming up food");
+            Thread.Sleep(time);
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("The food is warmed up");
+        }
+    }
+
+
+    internal class TVOnCommand : ICommand
+    {
+        TV tv;
+
+        public TVOnCommand(TV tv)
+        {
+            this.tv = tv;
+        }
+
+        public void Excute()
+        {
+            tv.On();
+        }
+
+        public void Undo()
+        {
+            tv.Off();
+        }
+    }
+
+
+    internal class MicrowaveCommand : ICommand
+    {
+        Microwave microwave;
+        int time;
+
+        public MicrowaveCommand(Microwave microwave, int time)
+        {
+            this.microwave = microwave;
+            this.time = time;
+        }
+
+        public void Excute()
+        {
+            microwave.Start(time);
+            microwave.Stop();
+        }
+
+        public void Undo()
+        {
+            microwave.Stop();
+        }
+    }
+
+    internal class NotebookCommand : ICommand
+    {
+        Notebook note;
+        int time;
+
+        public NotebookCommand(Notebook note)
+        {
+            this.note = note;
+        }
+
+        public void Excute()
+        {
+            note.On();
+        }
+
+        public void Undo()
+        {
+            note.Off();
+        }
+    }
+
+
+    internal class Controller
+    {
+        public ICommand cmd { get; set; }
+
+        public void PressButton()
+        {
+            if (cmd != null)
+            {
+                cmd.Excute();
+            }
+        }
+
+        public void PressUndo()
+        {
+            if (cmd != null)
+            {
+                cmd.Undo();
+            }
+        }
+    }
+}
---
+++ Volkov_HW_13_UML/Volkov_HW_13_UML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Volkov_HW_13_UML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            ICommand cmd = new MicrowaveCommand(microwave, 1000);
+            Controller control = new Controller();
+            control.cmd = cmd;
+            control.PressButton();
+
+            TV tv = new TV();
+            cmd = new TVOnCommand(tv);
+            control.cmd = cmd;
+            control.PressButton();
+            Thread.Sleep(1000);
+            control.PressUndo();
+
+            Notebook note = new Notebook();
+            cmd = new NotebookCommand(note);
+            control.cmd = cmd;
+            control.PressButton();
+            Thread.Sleep(1000);
+            control.PressUndo();
+        }
+    }
+}
