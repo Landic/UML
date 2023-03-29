49c36d4939e1aec04528521afb1b959982c1500e
Landic <danil.volkov.235@gmail.com>
2023-03-29 13:12:10 +0300

    Добавил дз(1) за 29.03.2023

---
+++ Volkov_HW_UML_12/Volkov_HW_UML_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler bank = new BankPaymentHandler();
+            Handler money = new MoneyPaymentHandler();
+            Handler paypal = new PaypalPaymentHandler();
+
+            bank.handler = paypal;
+            bank.handler = money;
+
+
+            Receiver rec = new Receiver(false, true, false);
+            Request obj = new Request();
+            obj.Req(bank, rec);
+            Receiver rec2 = new Receiver(true, false, false);
+            obj.Req(bank, rec2);
+            Receiver rec3 = new Receiver(false, false, true);
+            obj.Req(bank, rec3);
+        }
+    }
+}
---
+++ Volkov_HW_UML_12/Volkov_HW_UML_12/Receiver.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_12
+{
+    internal class Receiver
+    {
+        public bool BankTransfer { get; set; }
+        public bool MoneyTransfer { get; set; }
+        public bool PayPalTransfer { get; set; }
+
+        public Receiver(bool bankTransfer, bool moneyTransfer, bool payPalTransfer)
+        {
+            BankTransfer = bankTransfer;
+            MoneyTransfer = moneyTransfer;
+            PayPalTransfer = payPalTransfer;
+        }
+    }
+
+
+    internal abstract class Handler
+    {
+        public Handler handler { get; set; }
+
+        public abstract void Handle(Receiver obj);
+    }
+
+    internal class BankPaymentHandler : Handler
+    {
+        public override void Handle(Receiver obj)
+        {
+            if (obj.BankTransfer)
+            {
+                Console.WriteLine("Bank transfer");
+            }
+            else if (handler != null)
+            {
+                handler.Handle(obj);
+            }
+        }
+    }
+
+    internal class MoneyPaymentHandler : Handler
+    {
+        public override void Handle(Receiver obj)
+        {
+            if (obj.MoneyTransfer)
+            {
+                Console.WriteLine("Transfer through money transfer systems");
+            }
+            else if (handler != null)
+            {
+                handler.Handle(obj);
+            }
+        }
+    }
+
+
+    internal class PaypalPaymentHandler : Handler
+    {
+        public override void Handle(Receiver obj)
+        {
+            if (obj.PayPalTransfer)
+            {
+                Console.WriteLine("Transfer via paypal");
+            }
+            else if (handler != null)
+            {
+                handler.Handle(obj);
+            }
+        }
+    }
+
+    internal class Request
+    {
+        public void Req(Handler obj1, Receiver obj2)
+        {
+                obj1.Handle(obj2);
+        }
+    }
+}
