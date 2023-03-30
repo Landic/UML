13c5a7cfb06407865a83cd637f9b7e7d13675cfa
Landic <danil.volkov.235@gmail.com>
2023-03-30 19:50:21 +0300

    Добавил дз(2) за 30.03.2023

---
+++ Volkov_HW_UML_15/Volkov_HW_UML_15/Iterator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_15
+{
+    internal abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        public abstract int Key();
+
+        public abstract object Current();
+
+        public abstract bool MoveNext();
+
+        public abstract void Reset();
+    }
+
+    internal abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+
+    internal class RomaniaOrderIterator : Iterator
+    {
+        RomaniaCollection collection;
+
+        int position = -1;
+
+        bool reverse = false;
+
+        public RomaniaOrderIterator(RomaniaCollection collection, bool reverse = false)
+        {
+            this.collection = collection;
+            this.reverse = reverse;
+            if (reverse)
+            {
+                this.position = collection.GetItems().Count;
+            }
+        }
+
+        public override object Current()
+        {
+            return this.collection.GetItems()[position];
+        }
+
+        public override int Key()
+        {
+            return this.position;
+        }
+
+        public override bool MoveNext()
+        {
+            int update = this.position + (this.reverse ? -1 : 1);
+            if(update >= 0 && update < this.collection.GetItems().Count)
+            {
+                position = update;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Reset()
+        {
+            this.position = this.reverse ? this.collection.GetItems().Count - 1 : 0;
+        }
+    }
+
+    internal class RomaniaCollection : IteratorAggregate
+    {
+        List<string> list;
+        bool dir;
+
+
+        public RomaniaCollection()
+        {
+            list = new List<string>();
+            dir = false;
+        }
+
+        public void ReverseDirection()
+        {
+            dir = !dir;
+        }
+
+        public List<string> GetItems()
+        {
+            return list;
+        }
+
+        public void AddAttractions(string attractions)
+        {
+            list.Add(attractions);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new RomaniaOrderIterator(this, dir);
+        }
+    }
+}
---
+++ Volkov_HW_UML_15/Volkov_HW_UML_15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RomaniaCollection collection = new RomaniaCollection();
+            collection.AddAttractions("Колизей");
+            collection.AddAttractions("Фонтан Треви");
+            collection.AddAttractions("Пантеон");
+
+            foreach(var i in collection)
+            {
+                Console.WriteLine(i);
+            }
+
+            if (collection.GetItems().Contains("Колизей"))
+            {
+                Console.WriteLine("Пешком по навигатору 2 часа\nЕсли у вас есть бюджет можете потратить на экскурсовода и доберетесь за 50 минут");
+            }
+        }
+    }
+}
