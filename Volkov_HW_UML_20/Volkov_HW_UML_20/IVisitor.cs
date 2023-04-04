aee2ce30d758b8b64f164cfb53e2a4c7e8628edb
Landic <danil.volkov.235@gmail.com>
2023-04-04 11:17:24 +0300

    Добавил дз за 04.04.2023

---
+++ Volkov_HW_UML_20/Volkov_HW_UML_20/IVisitor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_20
+{
+    internal interface IVisitor
+    {
+        void VisitHouse(House house);
+        void VisitFactory(Factory factory);
+
+        void VisitBank(Bank bank);
+    }
+
+    internal interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    internal class House : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitHouse(this);
+        }
+    }
+
+    internal class Factory : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitFactory(this);
+        }
+    }
+
+    internal class Bank : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitBank(this);
+        }
+    }
+
+    internal class Agent : IVisitor
+    {
+        public void VisitBank(Bank bank)
+        {
+            Console.WriteLine("Предложение оформить страховку от грабежа");
+        }
+
+        public void VisitFactory(Factory factory)
+        {
+            Console.WriteLine("Предложение оформить страховку предприятия от пожара и наводнения");
+        }
+
+        public void VisitHouse(House house)
+        {
+            Console.WriteLine("Предложение оформить медицинскую страховку");
+        }
+    }
+
+
+    internal  class Client
+    {
+        public static void ClientCode(IElement element, IVisitor visitor)
+        {
+            element.Accept(visitor);
+        }
+    }
+}
---
+++ Volkov_HW_UML_20/Volkov_HW_UML_20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_UML_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IElement house = new House();
+
+            IVisitor visitor = new Agent();
+
+            Client.ClientCode(house, visitor);
+
+            IElement bank = new Bank();
+
+            Client.ClientCode(bank, visitor);
+
+            IElement factory = new Factory();
+
+            Client.ClientCode(factory, visitor);
+        }
+    }
+}
